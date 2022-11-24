13be94ffcfbfcafbb3dc03a8411ab2bde23f4140
Victoria Beasley <iesnoth91@gmail.com>
2022-11-24 15:11:27 -0600

    fixed gallery item page

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,17 +1,50 @@
-import GalleryItem from './GalleryItem'
+import { useState } from 'react'
 
-function Gallery(props){
+function GalleryItem(props){
+    let [view, setView] = useState(false)
 
-    const display = props.data.map((item,index) => {
+    const simpleView = () => {
         return (
-            <GalleryItem item={item} key={index} />
+            <div style={{
+                'width': '25vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative'
+            }}>
+                <h3>{props.item.trackName}</h3>
+                <h4>{props.item.collectionName}</h4>
+            </div>
         )
-    })
+    }
+
+    const detailView = () => {
+        return (
+            <div style={{
+                'width': '80vw',
+                'height': '20vh',
+                'border': '1px solid black',
+                'margin' : '2px',
+                'position': 'relative',
+                'backgroundImage': `url(${props.item.artworkUrl100})`,
+                'backgroundRepeat': 'no-repeat',
+                'backgroundSize': 'cover',
+                'color': 'white'
+            }}>
+                <h2>{props.item.trackName}</h2>
+                <h3>{props.item.collectionName}</h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            {display}
+        <div onClick={() => setView(!view)}
+        style={{'display': 'inline-block'}}>
+            {view ? detailView() : simpleView()}
         </div>
     )
-}
+} 
 
-export default Gallery
+export default GalleryItem
